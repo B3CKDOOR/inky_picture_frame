39b143c8f37804130304235d8bcea1580cc64478
B3CKDOOR <git@b3ckdoor.com>
2025-07-15 17:37:53 +0200

    Replace "Explode" deprication

--- selectimage.php
+++ selectimage.php
@@ -1,14 +1,18 @@
 <?php
-	$path = "mailpics"; // define foldername of the images
-	$fullpath = "/frame/$path/"; 
-    $mailimg_arr = explode("\n", shell_exec("sudo /bin/ls $path -I '*txt'")); // get all images in the folder as an array
-	
-	if(!empty($_GET['displayimg'])) { // when an image is clicked, display it on the picture frame
-		$img_arg = $fullpath . escapeshellcmd($_GET['displayimg']); 
-		shell_exec("sudo /frame/image.py $img_arg > /dev/null &"); // the image already has the right size so here we don't have to use anyimage.py
-		shell_exec("cp $img_arg /frame/recentimage/recent.jpg > /dev/null &"); // copy the image to the 'recentimage' folder so that we can revert back to it
-		$success = "<div class='alert alert-success' role='alert'><strong>Picture is being displayed!</strong><hr>This can take a little while.</div>";
-	}
+        $path = "mailpics"; // define foldername of the images
+        $fullpath = "/frame/$path/";
+
+        $mailimg_arr = scandir($path);
+        foreach($mailimg_arr as $file) {
+        }
+
+
+        if(!empty($_GET['displayimg'])) { // when an image is clicked, display it on the picture frame
+                $img_arg = $fullpath . escapeshellcmd($_GET['displayimg']);
+                shell_exec("sudo /frame/image.py $img_arg > /dev/null &"); // the image already has the right size so here we don't have to use anyimage.py
+                shell_exec("cp $img_arg /frame/recentimage/recent.jpg > /dev/null &"); // copy the image to the 'recentimage' folder so that we can revert back to it
+                $success = "<div class='alert alert-success' role='alert'><strong>Picture is being displayed!</strong><hr>This can take a little while.</div>";
+        }
 ?>
 
 <!doctype html>
@@ -16,7 +20,7 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
     <link href="bootstrap.min.css" rel="stylesheet">
@@ -26,18 +30,18 @@
   <body>
     <div class="container-sm" style="padding:20px;">
     <h1>Show picture</h1>
-	
-	<?php
-	if(!empty($success)){
-		echo $success;
-	}
-	foreach($mailimg_arr as $img) { // list the pictures and link them to the GET parameter which triggers script execution
-		if(!empty($img)) {
-			echo "<a href='?displayimg=$img'><img src='$path/$img' class='float-start img-thumbnail img-fluid rounded' style='max-width:150px;margin:5px;'></a>\n";
-		}
-	}
-	?>
-	</div>
+
+        <?php
+        if(!empty($success)){
+                echo $success;
+        }
+        foreach($mailimg_arr as $img) { // list the pictures and link them to the GET parameter which triggers script execution
+                if(!empty($img)) {
+                        echo "<a href='?displayimg=$img'><img src='$path/$img' class='float-start img-thumbnail img-fluid rounded' style='max-width:150px;margin:5px;'></a>\n";
+                }
+        }
+        ?>
+        </div>
 
   </body>
 </html>
