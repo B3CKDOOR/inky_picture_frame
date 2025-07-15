15021dcacb43038bccd8e2ac662ae3476046d00d
B3CKDOOR <git@b3ckdoor.com>
2025-07-15 19:47:46 +0200

    Update venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /venv
+set -gx VIRTUAL_ENV /frame/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
